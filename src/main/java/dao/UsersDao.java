8707bc298874632f7ee806d4f006ee790352b77f
bvska <pantera24816@gmail.com>
2021-01-07 16:42:55 +0300

    1

--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -2,9 +2,20 @@ package dao;
 
 import sqlEntity.Users;
 
+import javax.persistence.EntityManager;
+
+
 public class UsersDao implements Dao<Users, Integer>{
+
+    private EntityManager manager;
+
+    public UsersDao(EntityManager manager) {
+        this.manager = manager;
+    }
+
     @Override
     public void add(Users users) {
+        manager.persist(users);
 
     }
 
